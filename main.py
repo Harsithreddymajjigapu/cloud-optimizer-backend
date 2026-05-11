0d15850ac713ad0c1441758391c35fe4f27bbe6f
Harsithreddymajjigapu <majjigapuharsithreddy@gmail.com>
2026-05-11 11:18:54 +0530

    refining the code

---
+++ main.py
@@ -0,0 +1,53 @@
+from fastapi import FastAPI, Depends, HTTPException
+from sqlalchemy.orm import Session
+from typing import List
+import models
+import schemas
+from database import engine, get_db
+
+models.Base.metadata.create_all(bind=engine)
+
+app = FastAPI(title="Cloud Optimizer API")
+
+
+@app.post("/users/", response_model=schemas.UserResponse)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    """
+    1. The Bouncer (user: schemas.UserCreate) checks the incoming JSON format.
+    2. Depends(get_db) wakes up the Waiter to handle the database.
+    """
+    existing_user = db.query(models.User).filter(models.User.email == user.email).first()
+    if existing_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
+    new_user = models.User(email=user.email, department=user.department)
+    
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    
+    return new_user
+
+@app.get("/users/", response_model=List[schemas.UserResponse])
+def get_all_users(db: Session = Depends(get_db)):
+    """
+    Tell the Waiter to fetch every row from the Users table.
+    """
+    return db.query(models.User).all()
+
+
+@app.post("/servers/", response_model=schemas.CloudResourceResponse)
+def create_server(server: schemas.CloudResourceCreate, db: Session = Depends(get_db)):
+    """
+    Creates a new cloud server and links it to a User.
+    """
+    owner = db.query(models.User).filter(models.User.id == server.owner_id).first()
+    if not owner:
+        raise HTTPException(status_code=404, detail="User not found! Cannot assign a server to nobody.")
+    
+    new_server = models.CloudResource(**server.model_dump())
+    
+    db.add(new_server)
+    db.commit()
+    db.refresh(new_server)
+    
+    return new_server
