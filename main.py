d3d40b40446ce66dd05f208327fb3d19f324ea7c
Harsithreddymajjigapu <majjigapuharsithreddy@gmail.com>
2026-05-11 15:25:40 +0530

    refining the code

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ import models
 import schemas
 from database import engine, get_db
 
+
+from worker import analyze_server_efficiency 
+
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Cloud Optimizer API")
@@ -12,23 +15,19 @@ app = FastAPI(title="Cloud Optimizer API")
 
 @app.post("/users/", response_model=schemas.UserResponse)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    
     existing_user = db.query(models.User).filter(models.User.email == user.email).first()
     if existing_user:
         raise HTTPException(status_code=400, detail="Email already registered")
-    new_user = models.User(email=user.email, department=user.department)
     
+    new_user = models.User(email=user.email, department=user.department)
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
-    
     return new_user
 
 @app.get("/users/", response_model=List[schemas.UserResponse])
 def get_all_users(db: Session = Depends(get_db)):
-    
     return db.query(models.User).all()
-
 
 @app.post("/servers/", response_model=schemas.CloudResourceResponse)
 def create_server(server: schemas.CloudResourceCreate, db: Session = Depends(get_db)):
@@ -38,9 +37,10 @@ def create_server(server: schemas.CloudResourceCreate, db: Session = Depends(get
         raise HTTPException(status_code=404, detail="User not found! Cannot assign a server to nobody.")
     
     new_server = models.CloudResource(**server.model_dump())
-    
     db.add(new_server)
     db.commit()
     db.refresh(new_server)
+    
+    analyze_server_efficiency.delay(new_server.id, new_server.average_cpu_usage_percent)
     
     return new_server
