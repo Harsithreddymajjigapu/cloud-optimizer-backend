4496ce280702612590715bee45fa39f5034dc4ff
Harsithreddymajjigapu <majjigapuharsithreddy@gmail.com>
2026-05-13 12:53:23 +0530

    refining the code

--- Worker.py
+++ Worker.py
@@ -16,23 +16,45 @@ celery_app = Celery(
 
 @celery_app.task
 def analyze_server_efficiency(server_id: int, cpu_usage: float):
-    print(f"--- AI Worker: Processing Server {server_id} ---")
+    print(f"\n--- AI Worker: Waking up! Processing Server {server_id} ---")
+    
     time.sleep(10) 
     
-    advice = f"Server {server_id} is using {cpu_usage}% CPU. Recommendation: Reduce instance size to save 30%."
+    if cpu_usage < 20:
+        advice = f"Server is severely underutilized at {cpu_usage}%. Downgrade to save costs."
+        savings = 30.00
+        cli_command = "aws ec2 modify-instance-attribute --instance-type t3.micro"
+    elif cpu_usage > 80:
+        advice = f"Server is overloaded at {cpu_usage}%. Upgrade immediately to prevent crashes."
+        savings = 0.00 
+        cli_command = "aws ec2 modify-instance-attribute --instance-type t3.large"
+    else:
+        advice = f"Server is running efficiently at {cpu_usage}%."
+        savings = 0.00
+        cli_command = "None needed. Server is healthy."
+        
+    print(f"--- AI Worker: Finished thinking. Generating report... ---")
     
-    print(f"--- AI Worker: Finished! Advice: {advice} ---")
-    
-    # 2. NEW: Open the database and save the advice
     db = SessionLocal()
     try:
-        # Find the exact server in the database
         server = db.query(models.CloudResource).filter(models.CloudResource.id == server_id).first()
+        
         if server:
-            server.ai_advice = advice  # Put the advice in the mailbox
-            db.commit()                # Hit Save!
-            print("--- AI Worker: Saved to Database! ---")
+            
+            new_alert = models.OptimizationAlert(
+                resource_id=server.id,
+                ai_recommendation=advice,
+                estimated_monthly_savings=savings,
+                cli_command_to_fix=cli_command
+            )
+            
+            db.add(new_alert)  
+            db.commit()        
+            
+            print(f"--- AI Worker: SUCCESS! Saved new alert for Server {server_id} to the database. ---\n")
+        else:
+            print(f"--- AI Worker: ERROR! Could not find Server {server_id} in the database. ---\n")
+            
     finally:
-        db.close() # Always close the connection
-
+        db.close() 
     return advice
